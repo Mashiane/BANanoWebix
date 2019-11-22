12e45bdaebc4411d3fc1cfa66dd52cfaabf3d451
Mashy <mbanga.anele@gmail.com>
2019-11-22 10:06:38 +0200

    Webix 7.0.1

---
+++ BANanoWebix/Files/mini.skin.js
@@ -0,0 +1,3 @@
+//[Skin Customization]
+webix.skin.mini.barHeight=36;webix.skin.mini.tabbarHeight=34;webix.skin.mini.rowHeight=28;webix.skin.mini.listItemHeight=28;webix.skin.mini.inputHeight=30;webix.skin.mini.layoutMargin.wide=5;webix.skin.mini.layoutMargin.space=5;webix.skin.mini.layoutPadding.space=5;
+ webix.skin.set('mini');
